3dde40c6239a84fbdfbe9a571cf0bdd6b5d1e72a
GANZO9055 <kosta.maslihov@gmail.com>
2024-08-10 21:21:09 +0300

    4. Режим ожидания.

---
+++ src/main/java/ru/job4j/concurrent/Wget.java
@@ -0,0 +1,22 @@
+package ru.job4j.concurrent;
+
+public class Wget {
+    public static void main(String[] args) {
+        Thread thread = new Thread(
+                () -> {
+                    try {
+                        int i = 0;
+                        while (i++ < 100) {
+                            Thread.sleep(1000);
+                            System.out.print("\rЗагрузка : " + i  + "%");
+                        }
+                        System.out.println();
+                        System.out.println("Загрузка завершена.");
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+        );
+        thread.start();
+    }
+}
